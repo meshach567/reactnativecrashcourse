136c29857abd1b1c887cdc00a6444249d363e300
meshach567 <arinzemeshach123@gmail.com>
2024-10-14 16:48:30 +0100

    redit all file to branch main

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+import React from "react";
 
 export default function App() {
-    <View style={styles.container}>
+    <View className="flex-1items-center justify-center bg-white" style={styles.container}>
         <Text>
             RootLayout
         </Text>
         <StatusBar style="auto" />
+        <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
 }
 
 export const styles = StyleSheet.create({
     container: {
-
+        display: 'flex',
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-<<<<<<< HEAD
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
   };
 };
-=======
-    presets: ['babel-preset-expo'],
-  };
-};
->>>>>>> 4f3a68ee821372aa5af7273306cea4036806d186
